aff0f08e32b2ce2d90c7cf8c4268d9dfecb889b3
Sujith-2193 <matluriarunsujith@gmail.com>
2025-08-23 02:35:02 +0530

    Update Projects.jsx

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -32,7 +32,7 @@ const Projects = () => {
               whileHover={{ scale: 1.02 }}
               initial={{ opacity: 0, y: 20 }}
               whileInView={{ opacity: 1, y: 0 }}
-              transition={{ duration: 0.5 }}
+              transition={{ duration: 0.8 }}
               viewport={{ once: true }}
             >
               <h3 className="text-xl md:text-2xl font-semibold mb-2">{project.title}</h3>
